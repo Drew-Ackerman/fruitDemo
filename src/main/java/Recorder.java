91a771cbe5881bdffba602a140a31d6acf74aa5e
Drew.Ackerman <mitcha12@live.com>
2022-08-12 07:39:06 -0600

    Initial commit

---
+++ src/main/java/Recorder.java
@@ -0,0 +1,14 @@
+import io.gatling.recorder.GatlingRecorder;
+import io.gatling.recorder.config.RecorderPropertiesBuilder;
+import scala.Option;
+
+public class Recorder {
+  public static void main(String[] args) {
+    RecorderPropertiesBuilder props = new RecorderPropertiesBuilder()
+      .simulationsFolder(IDEPathHelper.gradleSourcesDirectory.toString())
+      .resourcesFolder(IDEPathHelper.gradleResourcesDirectory.toString())
+            .simulationPackage("org.churchofjesuschrist.temple.qa");
+
+    GatlingRecorder.fromMap(props.build(), Option.apply(IDEPathHelper.recorderConfigFile));
+  }
+}
